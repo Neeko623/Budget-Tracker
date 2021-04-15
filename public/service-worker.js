4fdd4508e193ec9910154153b7d0b7dc5db7c0e1
Neeko Tang <neeko_tvxq@hotmail.com>
2021-04-15 14:17:40 -0400

    added file to cache

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+const FILES_TO_CACHE = [
+    "/",
+    "public/index.html",
+    "public/js/index.js",
+    "public/css/styles.css",
+    "public/icons/icon-72x72.png",
+    "public/icons/icon-96x96.png",
+    "public/icons/icon-128x128.png",
+    "public/icons/icon-144x144.png",
+    "public/icons/icon-152x152.png",
+    "public/icons/icon-192x192.png",
+    "public/icons/icon-384x384.png",
+    "public/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
